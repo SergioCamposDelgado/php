3f10fa672407fd92af02be5950c3971f370df0a1
Sergio Campos Delgado <campos.delgado.sergio@iescamas.es>
2025-09-22 10:38:29 +0200

    Realizado ejercicio 2

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 1</title>
-<!--Escribe un programa que muestre tu nombre por pantalla utilizando código PHP-->
+    <title>Ejercicio 2</title>
+<!--Modifica el programa anterior para que muestre tu dirección y tu número de teléfono, cada dato en una línea distinta. Usa HTML y PHP.-->
 </head>
 <body>
     <?php
         $nombre = "Sergio";
-        echo "<h1> Hola $nombre </h1>";
+        $direccion = "IES Camas Antonio-Brisquet";
+        $tel = "123-45-67-89";
+        echo "<h1> Hola $nombre </h1>"."<br>"."<h2> Tu direccion es: $direccion </h2>"."<br>"."<h3>Tu numero de telefonno es: $tel</h3>";
     ?>
 </body>
 </html>
