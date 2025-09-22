b9e0340e74bb214d3cca6f88264d512eff101026
Sergio Campos Delgado <campos.delgado.sergio@iescamas.es>
2025-09-22 10:50:24 +0200

    Ejercicio 3 realizado

--- index.php
+++ index.php
@@ -3,15 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 2</title>
-<!--Modifica el programa anterior para que muestre tu dirección y tu número de teléfono, cada dato en una línea distinta. Usa HTML y PHP.-->
+    <title>Ejercicio 3</title>
+<!--Escribe un programa que muestre por pantalla 10 palabras en inglés con su traducción al castellano en dos columnas (usa)-->
 </head>
 <body>
     <?php
-        $nombre = "Sergio";
-        $direccion = "IES Camas Antonio-Brisquet";
-        $tel = "123-45-67-89";
-        echo "<h1> Hola $nombre </h1>"."<br>"."<h2> Tu direccion es: $direccion </h2>"."<br>"."<h3>Tu numero de telefonno es: $tel</h3>";
+        echo "<table border = '1'>
+        <tr> <th> ESPAÑOL </th> <th> INGLÉS </th> </tr> 
+        <tr> <td>perro</td> <td>dog</td></tr>
+        <tr> <td>gato</td> <td>cat</td></tr>
+        <tr> <td>verde</td> <td>green</td></tr>
+        <tr> <td>arreglar</td> <td>fix</td></tr>
+        <tr> <td>aprobar</td> <td>pass</td></tr>
+        <tr> <td>manzana</td> <td>apple</td></tr>
+        <tr> <td>platano</td> <td>banana</td></tr>
+        <tr> <td>caballo</td> <td>horse</td></tr>
+        <tr> <td>cerdo</td> <td>pig</td></tr>
+        <tr> <td>persona</td> <td>person</td></tr>
+        </table>";
+
     ?>
 </body>
 </html>
