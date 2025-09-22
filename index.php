77cca8e08494ba92c194a2faed35b8afc582a999
Sergio Campos Delgado <campos.delgado.sergio@iescamas.es>
2025-09-22 10:28:57 +0200

    Ejercicio 1 realizado

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+<!--Escribe un programa que muestre tu nombre por pantalla utilizando código PHP-->
+</head>
+<body>
+    <?php
+        $nombre = "Sergio";
+        echo "<h1> Hola $nombre </h1>";
+    ?>
+</body>
+</html>
