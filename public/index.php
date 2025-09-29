9b38cbd2a7b75d9487bb0ed4da500d29d960d9e7
SergioCamposDelgado <campos.delgado.sergio@iescamas.es>
2025-09-29 10:03:25 +0200

    subido el ejercicio 3

--- public/index.php
+++ public/index.php
@@ -6,10 +6,10 @@
 <body>
     <?php
         $i = 5;
-        while ($i <= 100){
+        do {
             echo "El valor de i es " . $i ."<br/>";
             $i+=5;
-        }
+        } while ($i <= 100);
     ?>
 </body>
 </html>
