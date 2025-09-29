e258a87df83699f63a30de1e127f74be4de3e50e
SergioCamposDelgado <campos.delgado.sergio@iescamas.es>
2025-09-29 09:56:32 +0200

    subido el ejercicio 1

--- public/index.php
+++ public/index.php
@@ -5,23 +5,9 @@
 </head>
 <body>
     <?php
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 5; $i <= 100; $i+=5) {
             echo "El valor de i es " . $i ."<br/>";
         }
-
-        
-        echo "<br/>";
-        $j = 0;
-        while ($j < 10) {
-            echo "El valor de j es " . $j ."<br/>";
-            $j++;
-        }
-        echo "<br/>";
-        $x = 0;
-        do {
-            echo "El valor de x es " . $x ."<br/>";
-            $x++;
-        } while ($x < 10);
     ?>
 </body>
 </html>
