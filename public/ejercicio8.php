e864212555612ab3571ac45af8e70e5a05edead3
SergioCamposDelgado <campos.delgado.sergio@iescamas.es>
2025-10-01 14:42:19 +0200

    Terminado de refinar

--- public/ejercicio8.php
+++ public/ejercicio8.php
@@ -10,18 +10,22 @@
 
 
 
-    <form action="ejercicio8.php" method="get">
+    <form  method="post">
+        <label for="numero">Numero: </label>
         <input type="number" name="numero" />
         <input type="submit" value="Enviar" />
     </form>
     <?php
-    if (isset($_GET["numero"])) {
-        $n = $_GET["numero"];
-        echo "<table border='1'>";
-            for ($i = 1; $i <= 10; $i++) {
-                echo "<tr><td>".$n." x </td><td>".$i." = </td><td>". ($i*$n)."</td></tr>";
-            } 
+    if (isset($_POST["numero"])) {
+        $n = $_POST["numero"];
+        echo "<h2>Tabla del $n</h2>";
+        echo "<table>";
+        for ($i = 1; $i <= 10; $i++) {
+            echo "<tr><td>" . $n . " x </td><td>" . $i . " = </td><td>" . ($i * $n) . "</td></tr>";
+        }
         echo "</table>";
+    } else  {
+        echo "<h2>Numero no enviado</h2>";
     }
     ?>
 
